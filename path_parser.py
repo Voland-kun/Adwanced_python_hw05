abd612afd895f6033fe57e917d5d517a78354436
Andrey Volynets <voland.artist@gmail.com>
2023-07-27 04:21:05 +0400

    Update path_parser.py

--- path_parser.py
+++ path_parser.py
@@ -12,13 +12,16 @@ def path_parser(user_line : str) -> ():
 def path_parser_1(user_line : str) -> ():
     *path, file = user_line.split("\\")
     path = str("\\".join(path) + "\\")
-    if len(file.split(".")) > 1:
+    if len(file.split(".")) == 1:
+        name, extension = file, ""
+    elif len(file.split(".")) == 2:
         name, extension = file.split(".")
     else:
-        name, extension = file, ""
+        *name, extension = file.split(".")
+        name = str(".".join(name))
     return (path, name, extension)
 
 
-path = r"C:\%USERPROFILE%\Saved Games\CD Projekt Red\Cyberpunk 2077\save.sav"
+path = r"C:\%USERPROFILE%\Saved Games\CD Projekt Red\Cyberpunk 2077\sa.ve.sav"
 # path = r"C:\%USER.PROFILE%\Saved Games\CD Projekt Red\Cyberpunk 2077\save"
 print(path_parser_1(path))
